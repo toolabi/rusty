dc892e8fb08ab13949aaf248f120bc2fae9c6e17
toolabi <prds.toolabi@gmail.com>
2025-04-02 23:16:39 +0200

    number representaion

--- var/src/main.rs
+++ var/src/main.rs
@@ -1,5 +1,25 @@
 fn main() {
     const VAR : i32= 2000;
     // you cant change a const with mut keyword
-    println!("Hello, world!");
+    // shadowing
+    let x = 2;
+    println!("x {:?}",x );
+    let x = String::new();
+    println!("x {:?}",x );
+
+    // scalar and compound data types
+    // scalar(int(signed, unsigned), float, boolean, char)
+    // number representaion 
+    let num :i32 = 10_22; //decimal
+    println!("decimal {:?}",num );
+    let num :i32 = 0xff; //hex
+    println!("hex {:?}",num );
+    let num :i32 = 0o77 ;//octal
+    println!("octal {:?}",num );
+    let num :i32 = 0b1111_0000; //binary
+    println!("binary {:?}",num );
+    let num :u8 = b'A' ;//byte
+    println!("byte {:?}",num );
+
+
 }
