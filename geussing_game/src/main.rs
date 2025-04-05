d973175c90550af23a443b3d4af05757879323fd
toolabi <prds.toolabi@gmail.com>
2025-04-05 12:20:54 +0200

    ownership

--- geussing_game/src/main.rs
+++ geussing_game/src/main.rs
@@ -5,10 +5,12 @@ use rand::Rng;
 fn main() {
     println!("Hello, world!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("{}", secret_number);
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to load message");
+    println!("{}", guess);
     let mut numeric_guess: u32 = guess.trim().parse().expect("failed to parse the guess.");
     match numeric_guess.cmp(&secret_number) {
         Ordering::Equal => println!("congarts"),
