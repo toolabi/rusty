4d468052864f9dbdb0473a89a87ca28c1e52b0de
pardis-toolabi <pardis.tlbi@gmail.com>
2025-04-05 12:32:14 +0200

    clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    // memory
+    // stack(functions and fixed size variables) and heap(dynamic size variables)
+    // ownership
+    // 1. each value has one variable called owner in rust
+    // 2. there can be one owner at the time
+    // 3. owner out of scope => value is dropped
+
+    let s = String::from("99");
+    let newOwner = s;
+
+
+    // println!("{:?}", s);
+    // throws error
+    // |
+    // 9  |     let s = String::from("99");
+    //    |         - move occurs because `s` has type `String`, which does not implement the `Copy` trait
+    // 10 |     let newOwner = s;
+    //    |                    - value moved here
+    // 11 |
+    // 12 |     println!("{:?}", s);
+    //    |                      ^ value borrowed here after move
+    //    |
+
+    
+    let s = String::from("99");
+    let newOwner = s.clone();
+
+
+    println!("s: {:?}", s);
+
 }
