59137b01ddc9710d100fb9757ffb21437b1718ce
toolabi <prds.toolabi@gmail.com>
2025-04-06 00:56:23 +0200

    option enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,13 +1,23 @@
-
-#[derive(Debug)]
 enum IpKind {
     V4(u8, u8, u8, u8),
-    V5(String)
+    V5(String),
 }
 
 fn main() {
-    let ip1 = IpKind::V4(127,0,0,1);
-    println!("{:?}", ip1);
-    let ip2 =IpKind::V5(String::from("127.0.0.1"));
-    println!("{:?}", ip2);
+    let ip1 = IpKind::V4(127, 0, 0, 1);
+    let ip2 = IpKind::V5(String::from("127.0.0.1"));
+    {
+        // options
+        let a = Some(5);
+        let s = Some("string");
+        let n :Option<i32> = None;
+
+        println!("{:?}", a.unwrap_or(0));
+        match a {
+            Some(a) => println!("some {:?}", a),
+            None => println!("None ")
+            // for other senarios
+            // _ => None
+        }
+    }
 }
