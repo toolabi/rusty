ccc50808302093e77c76101171f70e23ca6eab98
toolabi <prds.toolabi@gmail.com>
2025-04-30 15:21:36 +0200

    interior mutability

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -87,4 +87,114 @@ fn main() {
         println!("{:?}", Rc::strong_count(&a));
 
     }
+
+    
+        // interior mutability 
+        // allows mutate data when there are immutable refrence to that data
+        // box inforces bowrowing rules at compile time
+        // refcell inforces bowrowing rules at compile time <= only in signle thread programs
+        
+        // you can have mutable value inside refCell<T> even when the  refCell<T> is imutable.
+        
+        // targe change a mut data thats been refrenced by in immutable data
+        let mut num = 10;
+        let num_ref = &num;
+
+        // this throws an error 
+        // cannot assign to `*num_ref`, which is behind a `&` reference
+        // *num_ref = 12;
+
+
+    
+}
+
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker <'a, T: Messenger>{
+    messanger: &'a T,
+    value:usize,
+    max: usize
+
+}
+
+impl<'a,T> LimitTracker<'a, T> 
+where T : Messenger
+{
+    pub fn new(messenger: &'a T, max: usize)->LimitTracker<'a, T>{
+        LimitTracker{
+            messanger :messenger,
+            value:0,
+            max: max
+        }
+
+    }
+
+    pub fn set_value(&mut self, value: usize){
+        self.value = value;
+
+        let div = value as f64 / self.max as f64;
+
+        if div > 1.0 {
+            self.messanger.send("Error: you are over your qouta");
+        }else if div > 0.9{
+            self.messanger.send("Warning: you used over 90 percent of your qouta");
+            
+        }else if div > 0.75 {
+            self.messanger.send("Warning: you used over 75 percent of your qouta");
+        }
+
+
+    }
+
+    
+}
+
+
+#[cfg(test)]
+mod tests{
+
+    use super::*;
+    use super::*;
+    use super::Messenger;
+    use std::cell::RefCell;
+
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(Vec::new()),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+            // fails at run time if you borrow twice
+            // self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+    
+    #[test]
+
+    fn test(){
+        let meesanger = MockMessenger::new();
+
+        let mut limit_tracker = LimitTracker::new(&meesanger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(meesanger.sent_messages.borrow().len(),1);
+        
+
+    }
+    
+
 }
